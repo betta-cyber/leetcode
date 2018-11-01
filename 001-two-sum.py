e58d213c5f155f1acd3e791c493e2fbf68a37d1e
LightStrawberry <l21828604@gmail.com>
2018-11-01 22:10:21 +0800

    add some

--- 001-two-sum.py
+++ 001-two-sum.py
@@ -9,7 +9,7 @@ class Solution(object):
         :rtype: List[int]
         """
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
+            for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    return [i, j];
-        return [-1, -1];
+                    return [i, j]
+        return [-1, -1]
