ceb2ef56ef536ae3070eead98b8172f0defa6c74
LightStrawberry <l21828604@gmail.com>
2018-11-28 23:33:43 +0800

    rm useless py

--- python/001-two-sum.py
+++ python/001-two-sum.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -13,3 +14,15 @@ class Solution(object):
                 if nums[i] + nums[j] == target:
                     return [i, j]
         return [-1, -1]
+
+
+class Solution1(object):
+    def twoSum(self, nums, target):
+        if len(nums) <= 1:
+            return False
+        buff_dict = {}
+        for i in range(len(nums)):
+            if nums[i] in buff_dict:
+                return [buff_dict[nums[i]], i]
+            else:
+                buff_dict[target - nums[i]] = i
