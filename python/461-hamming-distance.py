2768d120b734a31a13c59c75bed863de38303908
betta <l21828604@gmail.com>
2018-12-24 18:24:46 +0800

    add hamming distance

---
+++ python/461-hamming-distance.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        return bin(x^y).count('1')
