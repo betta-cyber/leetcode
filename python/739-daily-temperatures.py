14577cc542e06bc6cab2476a0b1796d9f638af24
betta <l21828604@gmail.com>
2019-04-17 11:55:12 +0800

    daily temperatures

---
+++ python/739-daily-temperatures.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def dailyTemperatures(self, T):
+        """
+        :type T: List[int]
+        :rtype: List[int]
+        """
+        N = len(T)
+        stack = []
+        res = [0] * N
+        for i, t in enumerate(T):
+            while stack and stack[-1][0] < t:
+                oi = stack.pop()[1]
+                res[oi] = i - oi
+            stack.append((t, i))
+        return res
