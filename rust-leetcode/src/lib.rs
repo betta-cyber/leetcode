cc24ffc10e20e500ec6d7037683ff4da39473d01
LightStrawberry <l21828604@gmail.com>
2019-09-05 23:50:07 +0800

    [ADD]add some rust code for leetcode

---
+++ rust-leetcode/src/lib.rs
@@ -0,0 +1,7 @@
+// #![feature(test)]
+// #![feature(proc_macro_hygiene)]
+
+pub use leetcode_prelude::{linkedlist, btree, leetcode_test, vec_string, ListNode, TreeNode, assert_eq_sorted};
+
+pub mod two_sum;
+pub mod add_two_numbers;
---
+++ rust-leetcode/src/two_sum.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::with_capacity(nums.len());
+        println!("{:#?}", map);
+        for (index, num) in nums.iter().enumerate() {
+            match map.get(&(target - num)) {
+                None => { map.insert(num, index); },
+                Some(sub_index) => { return vec![*sub_index as i32, index as i32] },
+            }
+        }
+        vec![]
+    }
+}
+
+pub struct Solution;
+
+#[cfg(test)]
+mod test {
+    use super::Solution;
+
+    #[test]
+    fn test() {
+        assert_eq!(Solution::two_sum(vec![2, 7, 11, 5], 9), vec![0, 1]);
+    }
+}
