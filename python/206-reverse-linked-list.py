a9837a86073d783bb2e7566fcc4a8ee34ae0c983
Betta <21828604@qq.com>
2020-12-03 12:18:36 +0800

    Update 206-reverse-linked-list.py

--- python/206-reverse-linked-list.py
+++ python/206-reverse-linked-list.py
@@ -21,3 +21,31 @@ class Solution(object):
         n = node.next
         node.next = pre
         return self._reverse(n, node)
+
+    
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+
+        if not head:
+            return None
+        tmp = []
+        while head:
+            tmp.append(head)
+            head = head.next
+
+        result = tmp.pop()
+        current = result
+
+        while tmp:
+            k = tmp.pop(-1)
+            current.next = k
+            k.next = None
+            current = k
+
+        return result
